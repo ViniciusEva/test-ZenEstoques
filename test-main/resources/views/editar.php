84ae136a0eee98f6598520e9b6fcf857b8446c3b
ViniciusEva <vinicius.evangelista.ds@gmail.com>
2026-03-26 12:42:29 -0300

    Add files via upload

---
+++ test-main/resources/views/editar.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de Ajuda - Recriação</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../css/editar.css">
+    <link rel="stylesheet" href="../css/navbar.css">
+    <script src="../js/ajuda.js"></script>
+    
+</head>
+<body>
+        <div class="container-fluid p-0 dashboard-container">
+            <div class="row g-0">
+            <aside class="col-12 col-md-2">
+                <?php include 'navbarLateral.php'; ?>
+            </aside>
+
+
+        <main class="content-area col-12 col-md-10">
+            <div class="help-container">
+        
+        <div class="help-card">
+            <div class="icon-wrapper">
+                <div class="icon-placeholder"><i class="bi bi-person-badge-fill"></i></div>
+            </div>
+            <div class="content">
+                <h3>Administrador</h3>
+                <p>Lorem ipsum dolor sit dim amet, mea regione diamet principes at. Cum no movi lorem ipsum dolor sit dim.</p>
+            </div>
+            <div class="pagination-dots">
+                <span class="dot active"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+            </div>
+        </div>
+
+        <div class="help-card">
+            <div class="icon-wrapper">
+                <div class="icon-placeholder"><i class="bi bi-person-fill"></i></div>
+            </div>
+            <div class="content">
+                <h3>Usuário Comum</h3>
+                <p>Lorem ipsum dolor sit dim amet, mea regione diamet principes at. Cum no movi lorem ipsum dolor sit dim.</p>
+            </div>
+            <div class="pagination-dots">
+                <span class="dot"></span>
+                <span class="dot active"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+            </div>
+        </div>
+        </main>
+    
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.min.js"></script>
+
+</body>
+</html>
